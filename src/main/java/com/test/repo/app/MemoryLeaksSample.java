9b63b1e8610c9ca388874b909761b642a3cd818a
Jay-ar Bautista <jay-ar.bautista@devfactory.com>
2020-01-05 00:22:59 +0800

    Added memory leak sample (#1)
    * CI setup
    
    * Added memory leak sample
    
    * Make gradlew executable
    
    * Added gradle wrapper

---
+++ src/main/java/com/test/repo/app/MemoryLeaksSample.java
@@ -0,0 +1,14 @@
+package com.test.repo.app;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MemoryLeaksSample {
+
+	public void test() throws IOException {
+		File file = new File("test.txt");
+		FileReader reader = new FileReader(file);
+		reader.read();
+	}
+}
