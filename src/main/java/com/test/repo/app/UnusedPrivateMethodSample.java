fb651a48fd2403a2bfca767a73750ac4be547129
Jay-ar Bautista <jay-ar.bautista@devfactory.com>
2020-01-07 20:32:00 +0800

    CC-873885: Remove unused private method

--- src/main/java/com/test/repo/app/UnusedPrivateMethodSample.java
+++ src/main/java/com/test/repo/app/UnusedPrivateMethodSample.java
@@ -8,16 +8,6 @@ public class UnusedPrivateMethodSample {
 		return "Test";
 	}
 
-	private String unusedInstanceMethod(boolean testValue, List<String> values) {
-		if (testValue) {
-			for (String value : values) {
-				if (value.length() == 5) {
-					return value;
-				}
-			}
-		}
-		return "This method can be removed";
-	}
 
 	private String unusedStaticMethod() {
 		return "This static method can be removed";
