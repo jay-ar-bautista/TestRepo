ee24dbe88517e915e88b9bc92f754c4a4927fcf2
Jay-ar Bautista <jay-ar.bautista@devfactory.com>
2020-01-07 19:59:04 +0800

    Added Dead Code Sample

--- src/main/java/com/test/repo/app/UnusedPrivateMethodSample.java
+++ src/main/java/com/test/repo/app/UnusedPrivateMethodSample.java
@@ -1,16 +1,24 @@
 package com.test.repo.app;
 
+import java.util.List;
+
 public class UnusedPrivateMethodSample {
 
-	
-	public  String testMethod() {
+	public String testMethod() {
 		return "Test";
 	}
-	
-	private String unusedInstanceMethod() {
+
+	private String unusedInstanceMethod(boolean testValue, List<String> values) {
+		if (testValue) {
+			for (String value : values) {
+				if (value.length() == 5) {
+					return value;
+				}
+			}
+		}
 		return "This method can be removed";
 	}
-	
+
 	private String unusedStaticMethod() {
 		return "This static method can be removed";
 	}
