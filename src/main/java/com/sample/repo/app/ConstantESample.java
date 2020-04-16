3ff8f161d1f379645ca95047349c1194ce9f4646
hakan-koksal <hakan.koksal.7@gmail.com>
2020-04-16 13:25:22 +0100

    More constant samples added

--- src/main/java/com/sample/repo/app/ConstantESample.java
+++ src/main/java/com/sample/repo/app/ConstantESample.java
@@ -2,5 +2,18 @@ package com.sample.repo.app;
 
 public class ConstantESample {
 
-	public static String SHOULD_BE_CONSTANT = "should be constant";
+	public static String NAME = "admin";
+
+	public static final String PASSWORD = "password";
+
+	public static int customerCount = 0;
+
+	public static long executeTime = 1000L;
+
+	public static boolean isActive = false;
+
+	public static boolean hasChildren = true;
+
+	public static volatile int value = 1;
+	
 }
