984ed5cd19ecf714aece8085c6b2f53b79acd271
hakan-koksal <hakan.koksal.7@gmail.com>
2020-04-23 23:17:26 +0100

    MemoryLeaksSample new cases added

--- src/main/java/com/sample/repo/app/MemoryLeaksSample.java
+++ src/main/java/com/sample/repo/app/MemoryLeaksSample.java
@@ -6,9 +6,47 @@ import java.io.IOException;
 
 public class MemoryLeaksSample {
 
-	public void test() throws IOException {
-		File file = new File("test.txt");
-		FileReader reader = new FileReader(file);
-		reader.read();
-	}
+    public void test1() {
+        try {
+            File file = new File("test1.txt");
+            FileReader reader = new FileReader(file);
+            reader.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void test2() {
+        try {
+            File file = new File("test2.txt");
+            FileReader reader = new FileReader(file);
+            reader.read();
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void test3() {
+        try {
+            File file = new File("test3.txt");
+            FileReader reader = new FileReader(file);
+            reader.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            reader.close();
+        }
+    }
+
+    public void test4() {
+        try {
+            StringWriter stringWriter = new StringWriter();
+            stringWriter.append("text1");
+            stringWriter.append("text2");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
