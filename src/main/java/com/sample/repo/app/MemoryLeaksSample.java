e595eab9f7637a3f3e466b2bc65b1e082c313ec8
Hakan Köksal <56021820+hakan-kksl@users.noreply.github.com>
2020-04-24 09:52:52 +0100

    MemoryLeaksSample new cases added (#46)
    * MemoryLeaksSample new cases added
    
    * MemoryLeaksSample minor update
    
    * MemoryLeaksSample minor update
    
    * MemoryLeaksSample minor update
    
    * MemoryLeaksSample minor update
    
    * MemoryLeaksSample minor update
    
    * MemoryLeaksSample new case added
    
    Co-authored-by: hakan-koksal <hakan.koksal.7@gmail.com>

--- src/main/java/com/sample/repo/app/MemoryLeaksSample.java
+++ src/main/java/com/sample/repo/app/MemoryLeaksSample.java
@@ -3,12 +3,64 @@ package com.sample.repo.app;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringWriter;
 
 public class MemoryLeaksSample {
 
-	public void test() throws IOException {
-		File file = new File("test.txt");
-		FileReader reader = new FileReader(file);
-		reader.read();
-	}
+    public void test1() {
+        try {
+            File file = new File("test1.txt");
+            FileReader reader = new FileReader(file);
+            reader.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void test2() {
+        try {
+            File file = new File("test2.txt");
+            FileReader reader = new FileReader(file);
+            reader.read();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void test3() {
+        FileReader reader = null;
+        try {
+            File file = new File("test3.txt");
+            reader = new FileReader(file);
+            reader.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void test4() {
+        try {
+            StringWriter stringWriter = new StringWriter();
+            stringWriter.append("text1");
+            stringWriter.append("text2");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void test5() throws IOException {
+        File file = new File("test5.txt");
+        FileReader reader = new FileReader(file);
+        reader.read();
+    }
+
 }
